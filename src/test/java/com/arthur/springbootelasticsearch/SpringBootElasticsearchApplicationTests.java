92d1a524a460d6898961215d4c12e92ad80a1cbd
arthur <gogoke1@naver.com>
2019-03-09 23:41:28 +0900

    test case modify

--- src/test/java/com/arthur/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
+++ src/test/java/com/arthur/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class SpringBootElasticsearchApplicationTests {
     public void contextLoads() {
         List<Hotel> hotels = hotelRepository.findByCity("seoul");
 
-        System.out.println();
+        Assert.notEmpty(hotels, "Document Empty");
     }
 
 }
